db609b442c4a1c9c8fcc4864f32d0b93d267448b
Yakko Majuri <yakko.majuri@gmail.com>
2020-09-30 15:23:50 +0000

    added comments

--- scripts/feature-flags.js
+++ scripts/feature-flags.js
@@ -3,16 +3,19 @@ const resultElementFlag2 = document.getElementById('result-flag-2')
 const iceCreamSelectorElement = document.getElementById('select-ice-cream')
 const savePreferenceElement = document.getElementById('save-preference-btn')
 const loadingRingElement = document.getElementById('loading-ring')
+
 let loadingRingOn = false
 
+// Initialize PostHog tracking
 posthog.init(
     'sTMFPsFhdP1Ssg', 
     { 
         api_host: 'https://app.posthog.com', 
-        loaded: () => { updateFeatureFlagsDisplay(0) 
+        loaded: () => { updateFeatureFlagsDisplay(0)
     } 
 })
 
+// Update UI according to feature flags
 const updateFeatureFlagsDisplay = (timeout) => {
     setTimeout(() => {
         const isFeature1Enabled = posthog.isFeatureEnabled('demo-flag-1')
@@ -23,17 +26,19 @@ const updateFeatureFlagsDisplay = (timeout) => {
     
         resultElementFlag1['style']['color'] = isFeature1Enabled ? 'green' : 'red'
         resultElementFlag2['style']['color'] = isFeature2Enabled ? 'green' : 'red'
-        savePreferenceElement['className'] = 'btn btn-md ' + (isFeature2Enabled ? 'btn-success' : 'btn-primary')
+        savePreferenceElement['className'] = 'btn btn-md ' + (isFeature1Enabled ? 'btn-success' : 'btn-primary')
         console.log('[POSTHOG] Updated display')
     }, timeout)
 }
 
+// Display or hide loading animation
 const toggleLoadingRing = () => {
     if (loadingRingOn) loadingRingElement['style']['display'] = 'none'
     else loadingRingElement['style']['display'] = 'inline-block'
     loadingRingOn = !loadingRingOn
 }
 
+// Update user preference and reload feature flags
 const handlePreferenceChange = () => {
     toggleLoadingRing()
     posthog.people.set({ 'favorite_icecream': iceCreamSelectorElement.value })
@@ -53,6 +58,7 @@ const handlePreferenceChange = () => {
 
 savePreferenceElement.addEventListener('click', handlePreferenceChange)
 
+// PostHog listener for feature flag updates
 posthog.onFeatureFlags(() => {
     console.log('[POSTHOG] Feature flags reloaded successfully')
     console.log('[POSTHOG] Updating display...')
